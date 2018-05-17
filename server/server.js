ac4ca5342ee5a0a5ba3abc81fb34567a97cd0ac1
Affan Shahid <affan.shahid.94@gmail.com>
2018-05-18 04:43:04 +0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
+const ObjectId = mongoose.Types.ObjectId;
 const app = express();
 
 app.use(express.json());
@@ -25,6 +26,20 @@ app.post('/todos', async (req, res) => {
 app.get('/todos', async (req, res) => {
   const todos = await Todo.find();
   res.json({ todos });
+});
+
+app.get('/todos/:id', async (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectId.isValid(id)) return res.status(404).send();
+
+  try {
+    const todo = await Todo.findById(id);
+    if (!todo) return res.status(404).send();
+    res.json({ todo });
+  } catch (err) {
+    res.status(400).send();
+  }
 });
 
 module.exports = app;
